0d68a4472f50e398320546bf0b3c2e8e98a58ec2
Collins Swai <swailerc23@gmail.com>
2021-08-27 15:51:42 +0300

    Home fragment initialized

---
+++ app/src/main/java/com/example/bluez/fragments/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.bluez.fragments
+
+import android.app.ActionBar
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bluez.R
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var actionBar: ActionBar
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
