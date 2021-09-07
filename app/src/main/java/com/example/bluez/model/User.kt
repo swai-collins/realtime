59806b3743f3f2c68cb48e59b97a0669c4fe1514
Collins Swai <swailerc23@gmail.com>
2021-09-07 16:41:56 +0300

    Implemented users to app

--- app/src/main/java/com/example/bluez/model/User.kt
+++ app/src/main/java/com/example/bluez/model/User.kt
@@ -3,7 +3,9 @@ package com.example.bluez.model
 data class User(
     val id: String,
     val email: String,
-    val username: String
+    val username: String,
+    val phoneNumber: String,
+    val lastName: String
 ){
-    constructor():this("","","")
+    constructor():this("","","","","")
 }
